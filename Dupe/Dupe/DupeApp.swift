a5c7c07d44c55e34d7387881da7d138ec491caf3
Joshua James <jjvarghese@gmail.com>
2024-06-26 15:43:36 +0200

    master: add new project

---
+++ Dupe/Dupe/DupeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DupeApp.swift
+//  Dupe
+//
+//  Created by Varghese, Joshua on 26.06.24.
+//
+
+import SwiftUI
+
+@main
+struct DupeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
